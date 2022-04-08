af1397e0cbac983762ddc1fc07a89722daa30c2f
Alin <ursuleanalin@gmail.com>
2022-04-08 09:24:18 +0300

    users controllers

---
+++ Routes/Users.route.js
@@ -0,0 +1,17 @@
+const User = require("../controllers/Users.controller.js");
+
+const express = require('express')
+const app = express()
+
+module.exports = (app) => {
+
+    app.post("/register", User.create);
+
+    app.get("/Users", User.findAll);
+
+    app.put("/Users/:UserID", User.update);
+
+    app.delete("/Users/:UserID", User.delete);
+
+    app.get("/Users/:UserID", User.findOne);
+};
